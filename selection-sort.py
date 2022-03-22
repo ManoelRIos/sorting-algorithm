971510f3d0a2acbab8704230989e5da890ffccb9
ManoelRIos <manoel.rios@cfc.org.br>
2022-03-22 17:11:01 -0300

    Add explicação para selection sort

--- selection-sort.py
+++ selection-sort.py
@@ -1,6 +1,8 @@
 
+#Este algoritmo é baseado em se passar sempre o menor valor do vetor para a primeira posição 
+# (ou o maior dependendo da ordem requerida),
+# depois o segundo menor valor para a segunda posição e assim sucessivamente, até os últimos dois elementos. 
 
-from turtle import position
 import numpy as np
 import time 
 import pandas as pd
